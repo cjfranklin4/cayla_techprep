981f29723bbb8425a13709e48b9c073b34e25119
Cayla Franklin <cjfranklin4@gmail.com>
2023-03-25 18:33:25 -0500

    bootstrap set up

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+//Setup basic info on page load
+window.addEventListener("load", onPageLoad);
+
+function onPageLoad(){
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+    //getResults('Chicago');
+}
+
 const searchBox = document.querySelector('.search-box');
 searchBox.addEventListener('keypress', setQuery);
 
+//OpenWeather API Set Up
 const baseURL = api.baseUrl;
 const apiKey = api.API_KEY;
+
+//Geolocation API
 let locLat;
 let locLong;
 const activeLocation = document.querySelector('.get-location');
@@ -12,22 +25,10 @@ function getError(err){
     console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-function getResultsLocation(locLat, locLong){
-    fetch(`${baseURL}weather?lat=${locLat}&lon=${locLong}&units=metric&appid=${apiKey}`)
-        .then(weather => {
-            return weather.json();
-        }).catch(err => console.log(err))
-        .then(displayResults);
-}
-
 function getLocation(pos){
     const crd = pos.coords;
     locLat = crd.latitude;
     locLong = crd.longitude;
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-
     getResultsLocation(locLat, locLong);
 }
 
@@ -39,7 +40,7 @@ function enableLocation(){
     }
 }
 
-
+//OpenWeather API Calls
 function setQuery(evt){
     if(evt.keyCode == 13){
         getResults(searchBox.value);
@@ -54,6 +55,15 @@ function getResults(query){
         .then(displayResults);
 }
 
+function getResultsLocation(locLat, locLong){
+    fetch(`${baseURL}weather?lat=${locLat}&lon=${locLong}&units=metric&appid=${apiKey}`)
+        .then(weather => {
+            return weather.json();
+        }).catch(err => console.log(err))
+        .then(displayResults);
+}
+
+//Display OpenWeather API Data functions
 function displayResults(weather){
     console.log(weather);
     let city = document.querySelector('.location .city');
@@ -67,11 +77,21 @@ function displayResults(weather){
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
+    let feelsLike = document.querySelector('.current .feels-like');
+    feelsLike.innerHTML = `Feels Like: ${Math.round(weather.main.feels_like)}<span>°c</span>`;
+
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
+    let humidity = document.querySelector('.current .humidity');
+    humidity.innerText = `Humidity: ${weather.main.humidity}%`;
+
+    let weatherIcon = document.querySelector('.current .weather-icon');
+    weatherIcon.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+    weatherIcon.alt = weather.weather[0].main;
+
     let hiLow = document.querySelector('.hi-low');
-    hiLow.innerText = `${weather.main.temp_max}°c / ${weather.main.temp_min}°c`;
+    hiLow.innerText = `${Math.round(weather.main.temp_max)}°c / ${Math.round(weather.main.temp_min)}°c`;
 }
 
 function dateBuilder(d){
